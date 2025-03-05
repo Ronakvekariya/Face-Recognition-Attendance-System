3d5ac62e29bade12d23d251e4f42c1ee244470e9
Ronakvekariya <ronakk2108@gmail.com>
2025-03-05 15:57:02 +0530

    NEW UPDATE
    i have now finished the page where user can fill the form for the review of their absence

--- attendance/system/forms.py
+++ attendance/system/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import AbsenceReview
+# from .models import AbsenceReview
 
 class LoginForm(forms.Form):
     EmployeeId = forms.CharField(max_length=150)
@@ -7,12 +7,11 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class AbsenceReviewForm(forms.ModelForm):
-    class Meta:
-        model = AbsenceReview
-        fields = ['date', 'explanation']
-        widgets = {
-            'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'explanation': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
-        }
+class AbsenceReviewForm(forms.Form):
+    # The choices for this field will be set dynamically in the view.
+    date = forms.ChoiceField(choices=[], widget=forms.Select(attrs={'class': 'form-control'}))
+    explanation = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+        label="Explanation"
+    )
 
